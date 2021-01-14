72143024fd4e62b9687c2e9c9b7567f2c773c1cd
onsoim <onsoim@gmail.com>
2021-01-14 20:51:54 +0900

    [ADD] parse info from API
    I've added a parser of html code from API response.

---
+++ src/bot_discord.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import config
+import json
+import requests
+
+
+def run():
+    # with open("users.json", "r") as f: users = json.load(f)
+    users = {'MEMBERs': ['onsoim']}
+
+    msg = {}
+    for m in users['MEMBERs']:
+        info    = {}
+        url     = f'{config.URL_API}?boj={m}'
+        text    = requests.get(url).text
+
+        soup            = BeautifulSoup(text, 'html.parser')
+        info['tier']    = soup.select_one('svg > text.tier-text').get_text()
+        info['class']   = soup.select_one('svg > g:nth-child(6) > text.class.value').get_text()
+        info['solved']  = soup.select_one('svg > g:nth-child(7) > text.solved.value').get_text()
+        info['exp']     = soup.select_one('svg > g:nth-child(8) > text.something.value').get_text()
+        msg[m] = info
+    msg = json.dumps(msg, indent=4)
+
+    return msg
+
+
+if __name__ == "__main__":
+    run()
