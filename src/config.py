7e1fb0f92fa78f49528c0d6fd30f191e6e0b25ba
onsoim <onsoim@gmail.com>
2021-01-14 20:37:34 +0900

    [ADD] main code with its config code
    I've added two python codes. One for running main code for discord, the other for getting environment variables using in main code.

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+
+DISCORD_CHANNEL_NAME    = os.environ.get("DISCORD_CHANNEL_NAME") or ""
+DISCORD_GUILD_NAME      = os.environ.get("DISCORD_GUILD_NAME") or ""
+DISCORD_TOKEN           = os.environ.get("DISCORD_TOKEN") or ""
+
+URL_API                 = os.environ.get("URL_API") or ""
---
+++ src/run.py
@@ -0,0 +1,33 @@
+import config
+import discord
+
+
+def main():
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():        
+        channel_id = 0
+        for guild in client.guilds:
+            if guild.name == config.DISCORD_GUILD_NAME:
+                for channel in guild.channels:
+                    if channel.name == config.DISCORD_CHANNEL_NAME:
+                        channel_id = channel.id
+                        break
+            else: continue
+        channel = client.get_channel(channel_id)
+
+        import bot_discord
+        msg = bot_discord.run()
+        print(msg)
+
+        for index in range(0, len(msg), 2000):
+            await channel.send(msg[ index : index + 2000 ])
+
+        exit(0)
+
+    client.run(config.DISCORD_TOKEN)
+
+
+if __name__ == "__main__":
+    main()
