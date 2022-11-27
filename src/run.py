eb2c0b8b86c0e5764daf1479404326234cf685c3
onsoim <onsoim@gmail.com>
2022-11-27 12:43:18 +0900

    [UPDATE] discord.Client to discord.ext.commands.Bot

--- src/run.py
+++ src/run.py
@@ -1,5 +1,6 @@
 
-from datetime   import date
+from datetime       import date
+from discord.ext    import commands
 
 import discord
 
@@ -8,21 +9,22 @@ import config
 
 
 def main():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents = intents)
+    bot = commands.Bot(
+        command_prefix = '/',
+        intents = discord.Intents.all()
+    )
 
-    @client.event
-    async def on_ready():        
+    @bot.event
+    async def on_ready():
         channel_id = 0
-        for guild in client.guilds:
+        for guild in bot.guilds:
             if guild.name == config.DISCORD_GUILD_NAME:
                 for channel in guild.channels:
                     if channel.name == config.DISCORD_CHANNEL_NAME:
                         channel_id = channel.id
                         break
             else: continue
-        channel = client.get_channel(channel_id)
+        channel = bot.get_channel(channel_id)
 
         msg = f'```js\n[{date.today()}]\n{bot_discord.run()}\n```'
         print(msg)
@@ -32,7 +34,7 @@ def main():
 
         exit(0)
 
-    client.run(config.DISCORD_TOKEN)
+    bot.run(config.DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
