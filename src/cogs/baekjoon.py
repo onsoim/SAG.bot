52624f02980555c5cf41e6d3f26e8a9ce31f0edc
onsoim <onsoim@gmail.com>
2023-03-17 22:33:48 +0900

    [REFACTOR] update method into Baekjoon Cog

--- src/cogs/baekjoon.py
+++ src/cogs/baekjoon.py
@@ -11,71 +11,6 @@ sys.path.append('../')
 import config
 
 
-ERROR_USERS = []
-
-def simplified():
-    infos = dict(sorted(
-        json.loads(formatted()).items(),
-        key = lambda user: user[1]['rate'],
-        reverse=True
-    ))
-    msg   = []
-
-    for user in infos.keys():
-        info = infos[user]
-        msg += [ f"{user:12s} : {info['tier']:9s} ({info['percentage']:02d}%) {info['solved']:4d}" ]
-
-    if not len(ERROR_USERS):
-        return '\n'.join(msg)
-    return '\n'.join(msg) + f'\n[*] An Error Occured on "{", ".join(ERROR_USERS)}"'
-
-
-def formatted():
-    global ERROR_USERS
-    
-    with open("res/users.json", "r") as f: users = json.load(f)
-    regulate = {}
-
-    msg = {}
-    for m in users.keys() :
-        info    = {}
-        url     = f'{config.URL_API}?boj={m}'
-        res     = requests.get(url)
-
-        if res.status_code == 200:
-            soup                = BeautifulSoup(res.text, 'html.parser')
-            info['tier']        = soup.select_one('svg > text.tier-text').get_text()
-            info['rate']        = int(soup.select_one('svg > g:nth-child(6) > text.rate.value').get_text().replace(',', ''))
-            info['solved']      = int(soup.select_one('svg > g:nth-child(7) > text.solved.value').get_text().replace(',', ''))
-            info['class']       = soup.select_one('svg > g:nth-child(8) > text.class.value').get_text()
-            info['percentage']  = int(soup.select_one('svg > text.percentage').get_text()[ : -1 ])
-            msg[m] = info
-
-            if info['rate'] >= 1600:
-                regulate[users[m]] = 10
-            elif info['rate'] >= 1250:
-                regulate[users[m]] = 3
-            else:
-                regulate[users[m]] = 2
-
-        else:
-            ERROR_USERS        += [ m ]
-            regulate[users[m]] = 10
-
-    with open("data/regulation.json", "w") as f:
-        json.dump(regulate, f, indent=4)
-
-    return json.dumps(msg, indent=4)
-
-
-def run():
-    return simplified()
-
-
-if __name__ == "__main__":
-    print(run())
-
-
 class Baekjoon(Cog):
     def __init__(self, bot):
         print('init baekjoon cog')
@@ -93,11 +28,60 @@ class Baekjoon(Cog):
             else: continue
         channel = self.bot.get_channel(channel_id)
 
-        msg = f'```js\n[{date.today()}]\n{run()}\n```'
+        msg = f'```js\n[{date.today()}]\n{self.update()}\n```'
         print(msg)
 
         for index in range(0, len(msg), 2000):
             await channel.send(msg[ index : index + 2000 ])
+
+    def update(self):
+        infos       = {}
+        regulate    = {}
+        ERROR_USERS = []
+
+        with open("res/users.json", "r") as f: users = json.load(f)
+
+        for m in users.keys() :
+            info    = {}
+            res     = requests.get(f'{config.URL_API}?boj={m}')
+
+            if res.status_code == 200:
+                soup                = BeautifulSoup(res.text, 'html.parser')
+                info['tier']        = soup.select_one('svg > text.tier-text').get_text()
+                info['rate']        = int(soup.select_one('svg > g:nth-child(6) > text.rate.value').get_text().replace(',', ''))
+                info['solved']      = int(soup.select_one('svg > g:nth-child(7) > text.solved.value').get_text().replace(',', ''))
+                info['percentage']  = int(soup.select_one('svg > text.percentage').get_text()[ : -1 ])
+
+                infos[m] = info
+
+                if info['rate'] >= 1600:
+                    regulate[users[m]] = 10
+                elif info['rate'] >= 1250:
+                    regulate[users[m]] = 3
+                else:
+                    regulate[users[m]] = 2
+
+            else:
+                ERROR_USERS        += [ m ]
+                regulate[users[m]] = 10
+
+        with open("data/regulation.json", "w") as f:
+            json.dump(regulate, f, indent=4)
+
+        infos = dict(sorted(
+            infos.items(),
+            key = lambda user: user[1]['rate'],
+            reverse=True
+        ))
+
+        msg   = []
+        for user in infos.keys():
+            info = infos[user]
+            msg += [ f"{user:12s} : {info['tier']:9s} ({info['percentage']:02d}%) {info['solved']:4d}" ]
+
+        if not len(ERROR_USERS):
+            return '\n'.join(msg)
+        return '\n'.join(msg) + f'\n[*] An Error Occured on "{", ".join(ERROR_USERS)}"'
 
 
 async def setup(bot):
